2398cfde3b3abdc067b0ed3cec6154c7c789bd2a
Pavel Shvedov <uznick@ya.ru>
2013-12-24 11:29:47 +0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-sphinx-db',
-    version='1.3.2.1',
+    version='1.3.2.2',
     packages=[
         "django_sphinx_db",
         "django_sphinx_db.backend",
